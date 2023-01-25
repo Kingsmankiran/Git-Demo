517f05a071baf7fc0b54d680124918b7025d98eb
Kingsmankiran <koppollareddykiran@gmail.com>
2023-01-25 12:40:30 +0530

    "adding java files"

---
+++ src/com/kiran/Arthematic.java
@@ -0,0 +1,24 @@
+package com.kiran;
+
+public class Arthematic {
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int subtract(int a,int b){
+        return a-b;
+    }
+    public int mul(int a,int b){
+        return a*b;
+    }
+    public int div(int a,int b){
+        return a%b;
+    }
+    public static void main(String args[]){
+        Arthematic a1 = new Arthematic();
+        System.out.println("addition of two numbers: "+a1.add(5,6));
+        System.out.println("subtracting of two numbers: "+a1.subtract(5,6));
+        System.out.println("multiplication of two numbers: "+a1.mul(5,6));
+        System.out.println("division of two numbers: "+a1.div(5,6));
+    }
+}
+
